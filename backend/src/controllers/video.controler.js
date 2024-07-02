f1ad4b4ef6a019a0c5498f338237af44517c6d26
rohan5091 <rohanmalakar5091@gmail.com>
2024-07-02 12:53:35 +0530

    completed video controller

---
+++ backend/src/controllers/video.controler.js
@@ -0,0 +1,146 @@
+import asyncHandler from "../utils/asyncHandler";
+import User from "../models/User.js";
+import ApiError from "../utils/ApiError.js";
+import Video from "../moddels/video.model.js";
+import ApiResponse from "../utils/ApiResponse.js";
+
+const createVideo = asyncHandler(async (req, res,next) => {
+    
+     const { id } = req.user.id;
+      
+     const user= await User.findById(id);
+
+     if (!user) {
+       return ApiError(404,"User not found");
+     }
+
+     const { videoFile, thumbnail, title, description, category ,isPublished} = req.body;
+
+     if (!videoFile || !thumbnail || !title || !description || !category) {
+       return ApiError(400,"Please fill in all fields");
+      };
+
+
+      const video = {
+        videoFile,
+        thumbnail,
+        title,
+        description,
+        category,
+        owner: id,
+        isPublished,
+      };
+
+      const newVideo = new Video(video);
+      await newVideo.save();
+
+      ApiResponse(201,"Video created successfully",newVideo);
+
+});
+
+const updateVideo = asyncHandler(async (req, res,next) => {
+    
+  const { id } = req.user.id;
+  const user= await User.findById(id);
+
+  if (!user) {
+    return ApiError(404,"User not found");
+  }
+
+  const { title, description,isPublished,videoId} = req.body;
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    return ApiError(404,"Video not found");
+  }
+
+  if(video.owner.toString() !== id){
+    return ApiError(401,"You are not authorized to update this video");
+  }
+  
+  video.title = title;
+  video.description = description;
+  video.isPublished = isPublished;
+
+  await video.save();
+  
+   ApiResponse(201,"Video created successfully",video);
+
+});
+
+const deleteVideo = asyncHandler(async (req, res,next) => {
+    
+  const userId = req.user.id;
+  const user= await User.findById(userId);
+
+  if (!user) {
+    return ApiError(404,"User not found");
+  }
+
+  const {videoId} = req.body;
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    return ApiError(404,"Video not found");
+  }
+
+  if(video.owner.toString() !== id){
+    return ApiError(401,"You are not authorized to update this video");
+  }
+
+   await video.remove();
+
+   ApiResponse(201,"Video created successfully",video);
+
+});
+
+const getVideo = asyncHandler(async (req, res,next) => {
+    
+    const { id } = req.params;
+
+    const video = await Video.findById(id);
+
+    if (!video) {
+      return ApiError(404,"Video not found");
+    }
+    video.views+=1;
+
+    ApiResponse(200,"Video fetched successfully",video);
+
+});
+
+const getAllVideosOfUser = asyncHandler(async (req, res,next) => {
+
+  const { userId } = req.body;
+
+  const user= await User.findById(userId);
+
+  if (!user) {
+    return ApiError(404,"User not found");
+  }
+
+  const allVideo=await Video.find(owner=userId);
+
+  ApiResponse(201,"All videos detail",allVideo);
+
+});
+
+const getAllVideos=asyncHandler(async (req,res)=>{
+   
+   const allVideos=await Video.find();
+
+   if (!allVideos) {
+      ApiError(502,"server error");
+   }
+
+   ApiResponse(202,"All videos detels",allVideos);
+
+});
+
+
+
+
+export { createVideo, getVideo, updateVideo,deleteVideo,getAllVideosOfUser,getAllVideos};
+
