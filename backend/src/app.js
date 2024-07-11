7faf70e003f2eb277394bf16a606972244e07d34
rohan5091 <rohanmalakar5091@gmail.com>
2024-07-11 13:57:26 +0530

    changed some files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 import videoRouter from "./routes/video.route.js"
 import commentRouter from "./routes/comment.route.js"
 import tweetRouter from "./routes/tweet.route.js";
 import likeRouter from "./routes/like.route.js";
+import subscriptionRouter from "./routes/subscription.route.js";
+import userRouter from "./routes/user.route.js";
+import ApiError from "./utils/ApiError.js";
 
 
 const app=express();
@@ -18,12 +22,17 @@ app.use(
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
+app.use(cookieParser());
 
-app.use("api/v1/video",videoRouter);
-app.use("api/v1/comment",commentRouter);
-app.use("api/v1/tweet",tweetRouter);
-app.use("api/v1/like",likeRouter);
-app.use("api/v1/subscription",subscriptionRouter);
+app.use("/api/v1/video/",videoRouter);
+app.use("/api/v1/comment/",commentRouter);
+app.use("/api/v1/tweet/",tweetRouter);
+app.use("/api/v1/like/",likeRouter);
+app.use("/api/v1/subscription/",subscriptionRouter);
+app.use("/api/v1/user/",userRouter);
+app.use("*",(req,res,next)=>{
+   throw new ApiError(404,"route not found");
+})
 
 
 export {app};
--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,1 +1,5 @@
 export const DB_NAME="videotube";
+
+export const emailValidate="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+export const passwordValidate="^(?=.*[A-Z])(?=.*[\d!@#$%^&*]).*$";
